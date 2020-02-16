d9c0c34caf674897c2e698b03d2cbb4572555ff5
Mike <mikhailhusyev@gmail.com>
2020-02-16 00:58:30 -0600

    Initial Commit

---
+++ src/main/java/com/groupg/achfilevalidator/AchFileValidatorApplication.java
@@ -0,0 +1,13 @@
+package com.groupg.achfilevalidator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AchFileValidatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AchFileValidatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/groupg/achfilevalidator/AchFileValidatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.groupg.achfilevalidator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AchFileValidatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
