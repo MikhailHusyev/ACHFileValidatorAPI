0a85a690d3c8216323723cf4c1173e3451104359
Bo Varvil <bovarvil@gmail.com>
2020-03-23 20:03:22 -0500

    Re-did the Hash validation

--- src/main/java/com/groupg/achfilevalidator/services/validation/ValidationTests.java
+++ src/main/java/com/groupg/achfilevalidator/services/validation/ValidationTests.java
@@ -5,6 +5,39 @@ import com.groupg.achfilevalidator.models.ErrorResponse;
 
 public class ValidationTests {
 	public ErrorResponse validHash(ACHFile file) {
+		
+		int entryNum = file.getEntryDetail().size();
+		String fileHash = file.getFileControl().getEntryHash();
+		String batchHash = file.getCompanyBatchControl().getEntryHash();
+		String realHash = "";
+		int hashCalc = 0;
+		
+		for(int i = 0; i < entryNum; i++) {
+			hashCalc += Integer.valueOf(file.getEntryDetail().get(i).getReceivingRoutingNum());
+		}
+		
+		if(String.valueOf(hashCalc).length() < 10) {
+			int zeros = 10 - String.valueOf(hashCalc).length();
+			while (zeros > 0) {
+				realHash += "0";
+				zeros--;
+			}
+			realHash += "" + hashCalc;
+		} else if(String.valueOf(hashCalc).length() > 10) {
+			int start = String.valueOf(hashCalc).length() - 10;
+			
+			for(int i = start; i < 11; i++) {
+				realHash += String.valueOf(hashCalc).charAt(i);
+			}
+		}
+		
+		if(fileHash.compareTo(realHash) != 0)
+			return ErrorResponse.HASH_CODE_ERROR;
+		if(batchHash.compareTo(realHash) != 0)
+			return ErrorResponse.HASH_CODE_ERROR;
+		
+		
+		
 		return ErrorResponse.CLEAN_FILE_NO_ERROR;
 	}
 }
