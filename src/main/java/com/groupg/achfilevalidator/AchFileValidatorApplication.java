d246c5480906b15400f4e62febcb64d55150da4f
Mike <mikhailhusyev@gmail.com>
2020-04-26 23:29:09 -0500

    updated CORS permissions

--- src/main/java/com/groupg/achfilevalidator/AchFileValidatorApplication.java
+++ src/main/java/com/groupg/achfilevalidator/AchFileValidatorApplication.java
@@ -2,6 +2,7 @@ package com.groupg.achfilevalidator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
 public class AchFileValidatorApplication {
--- src/main/java/com/groupg/achfilevalidator/controllers/ValidationController.java
+++ src/main/java/com/groupg/achfilevalidator/controllers/ValidationController.java
@@ -5,13 +5,16 @@ import com.groupg.achfilevalidator.services.ValidationServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.core.io.InputStreamSource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @RequestMapping("api/v1/file/validate")
 @RestController
+@CrossOrigin(origins="*")
 public class ValidationController{
 
     @Autowired
@@ -19,7 +22,7 @@ public class ValidationController{
     private ValidationServiceImpl validationService;
 
     @PostMapping
-    public APIResponse validateFile(@RequestParam("file") InputStreamSource file){
+    public APIResponse validateFile(@RequestParam("file") MultipartFile file){
     	return validationService.validate(file);
     }
 }
